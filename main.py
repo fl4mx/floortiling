5b0f62b731c4a6b57238b50a0c7d37f5b85f9969
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2021-01-12 11:37:19 +1100

    Update main.py
    Patch for when consecutive endlengths are minimumgap, hence unstable. Add a delta, to rectify this issue and stagger the endlengths by delta length apart.

--- main.py
+++ main.py
@@ -2,30 +2,18 @@
 import numpy
 import matplotlib.pyplot as plt
 
-#initial problem details
-"""
-16 rows in total
-1st row = 318
-2nd row = 328
-3rd to 7th row = 340
-8th to 16th row = 328
-
-initial calculations:
-minimum wastage = 52, assuming perfect fit of 122
-minimum blocks = 11.42, ignoring >20 length restriction
-"""
-
 #init values
-rows = 16
-columns = 4
+rows = 26
+columns = 5
 piece = 122
 wastage = 0
-minimumgap = 20
+minimumgap = 25
+delta = 10
 leftover = 0
 
 #init arrays
 finalarray = numpy.zeros((int(rows), int(columns)))
-rowlengths = (numpy.array([318, 328, 340, 340, 340, 340, 340, 328, 328, 328, 328, 328, 328, 328, 328, 328]))
+rowlengths = (numpy.array([495, 495, 500, 500, 500, 500, 500, 500, 500, 435, 435, 435, 435, 435, 435, 435, 435, 435, 435, 435, 435, 435, 430, 430, 430, 430]))
 
 
 #iterate rows
@@ -33,17 +21,24 @@ for row in range(rows):
     if (leftover < minimumgap):
         wastage = wastage + leftover
         leftover = 0
-    endlength = (rowlengths[row] - leftover - (2 * piece))
+    endlength = (rowlengths[row] - leftover - (3 * piece))
     if (((endlength < minimumgap) & (endlength > 0)) | (((piece - abs(endlength)) < minimumgap))):
         difference = minimumgap - endlength
         front = leftover - difference
         endlength = minimumgap
-        wastage = wastage + difference
+        if (endlength == finalarray[row - 1][3]):
+            print("same")
+            endlength = endlength + delta
+            difference = minimumgap - endlength
+            #difference = endlength - minimumgap
+            front = leftover - difference
+        wastage = wastage + abs(difference)
         leftover = piece - minimumgap
         finalarray[row][0] = front
         finalarray[row][1] = piece
         finalarray[row][2] = piece
-        finalarray[row][3] = endlength
+        finalarray[row][3] = piece
+        finalarray[row][4] = endlength
     elif (endlength <= 0):
         front = leftover
         if abs(endlength) >= 20:
@@ -53,16 +48,21 @@ for row in range(rows):
             wastage = wastage + abs(endlength)
         #endlength is negative, so we add it
         endlength = piece + endlength
+        if (endlength == finalarray[row - 1][3]):
+            print("same")
+            endlength = endlength + delta
         finalarray[row][0] = front
         finalarray[row][1] = piece
-        finalarray[row][2] = endlength
+        finalarray[row][2] = piece
+        finalarray[row][3] = endlength
     elif (endlength > minimumgap):
         front = leftover
         leftover = piece - endlength
         finalarray[row][0] = front
         finalarray[row][1] = piece
         finalarray[row][2] = piece
-        finalarray[row][3] = endlength
+        finalarray[row][3] = piece
+        finalarray[row][4] = endlength
 
 
 #data structures for plotting
@@ -98,29 +98,29 @@ def plotter(arrangeDict, category_names):
     ax.invert_yaxis()
     ax.xaxis.set_visible(False)
     ax.set_xlim(0, numpy.sum(data, axis = 1).max())
-    print(data)
+    #print(data)
     y_pos = 40 * numpy.arange(len(labels))
-    print(y_pos)
+    #print(y_pos)
     for i, (colname, color) in enumerate(zip(category_names, category_colors)):
         widths = data[:, i]
-        print("widths")
-        print(widths)
+        #print("widths")
+        #print(widths)
         starts = data_cum[:, i] - widths
-        print("starts")
-        print(starts)
+        #print("starts")
+        #print(starts)
         ax.barh(y_pos, widths, left = starts, height = 40, label = colname, color = color)
         xcenters = starts + (widths / 2)
         r, g, b, _ = color
         text_color = 'white' if r * g * b < 0.2 else 'darkgrey'
         for y, (x, c) in enumerate(zip(xcenters, widths)):
             ax.text(x, y_pos[y], str(int(c)), ha = "center", va = "center", color = text_color)
-    ax.legend(ncol = len(category_names), bbox_to_anchor = (0, 1.035), loc = "center left", fontsize = "small")
+    ax.legend(ncol = len(category_names), bbox_to_anchor = (1.04, 1.02), loc = "center right", fontsize = "small")
     ax.set_yticks(y_pos)
     ax.set_yticklabels(labels)
     fig.suptitle("Floorboard Arrangement", fontsize = 20, fontweight = "bold")
-    ax.text(0.98, 1.06, ("Wastage = " + str(wastage)), verticalalignment = "bottom", horizontalalignment = "center", transform = ax.transAxes, color = 'green', fontsize = 8)
-    ax.text(0.98, 1.03, ("Minimum Gap = " + str(minimumgap)), verticalalignment = "bottom", horizontalalignment = "center", transform = ax.transAxes, color = 'green', fontsize = 8)
-    print(labels)
+    ax.text(0.98, 1.07, ("Wastage = " + str(wastage)), verticalalignment = "bottom", horizontalalignment = "center", transform = ax.transAxes, color = 'green', fontsize = 8)
+    ax.text(0.98, 1.04, ("Minimum Gap = " + str(minimumgap)), verticalalignment = "bottom", horizontalalignment = "center", transform = ax.transAxes, color = 'green', fontsize = 8)
+    #print(labels)
     return fig, ax
 
 #plot
